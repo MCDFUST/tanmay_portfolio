32707c86661e4ff6388cc7737755149cdaae85ab
MCDFUST <callmetjmax@gmail.com>
2024-11-05 00:54:11 +0530

    9

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProjectCard({h3, p, description }) {
   return (
-    <div style="width:25%">
+    <div style="width:25%;">
       <h3>{h3}</h3>
       <p>{p}</p>
       <p>{description}</p>
